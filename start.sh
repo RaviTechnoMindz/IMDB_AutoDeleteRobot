5aba43655c5ec178adfc7de69024fc919232b336
Ravinandan <104293257+RaviTechnoMindz@users.noreply.github.com>
2023-04-23 21:15:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TechnoMindz/IMDB_AutoDeleteRobot.git /IMDB_AutoDeleteRobot
+  git clone https://github.com/RaviTechnoMindz/IMDB_AutoDeleteRobot.git /IMDB_AutoDeleteRobot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /IMDB_AutoDeleteRobot
